e65b14104695e63563335331034d9c18d3242e04
Cristian Felipe <cristianpipe0494@gmail.com>
2019-07-24 12:25:01 -0600

    Have initialized repo, first commit. Uploaded template for PWA.

--- src/js/index.js
+++ src/js/index.js
@@ -6,9 +6,49 @@ import '../scss/style.scss';
 
 // Register service worker.
 import './registerServiceWorker';
+import { setInterval } from 'timers';
 
 dayjs.locale('es');
 const container = document.getElementById('container');
-const hour = document.createElement('p');
-hour.innerHTML = `Hora actual: ${dayjs().format('h:mm a')}`;
-container.appendChild(hour);
+
+const timer = document.createElement('div');
+timer.setAttribute('class', 'clock-ui');
+
+const timerHour = document.createElement('p');
+const timerMinute = document.createElement('p');
+timerMinute.setAttribute('class', 'ticker-ui');
+const timerState = document.createElement('p');
+
+// Format time.
+let actualHour = dayjs().format('h');
+let actualMinute = dayjs().format('mm');
+let actualState = dayjs().format('a');
+
+timerHour.innerHTML = actualHour;
+timerMinute.innerHTML = actualMinute;
+timerState.innerHTML = actualState;
+
+timer.appendChild(timerHour);
+timer.appendChild(timerMinute);
+timer.appendChild(timerState);
+
+// Update time and add new time.
+setInterval(() => {
+  timerHour.innerHTML = '';
+  timerMinute.innerHTML = '';
+  timerState.innerHTML = '';
+
+  actualHour = dayjs().format('h');
+  actualMinute = dayjs().format('mm');
+  actualState = dayjs().format('a');
+
+  timerHour.innerHTML = actualHour;
+  timerMinute.innerHTML = actualMinute;
+  timerState.innerHTML = actualState;
+
+  timer.appendChild(timerHour);
+  timer.appendChild(timerMinute);
+  timer.appendChild(timerState);
+}, 1000);
+
+container.appendChild(timer);
--- src/sw.js
+++ src/sw.js
@@ -2,5 +2,4 @@
 /* eslint no-underscore-dangle: 0 */
 /* eslint no-restricted-globals: 0 */
 /* global workbox */
-
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
